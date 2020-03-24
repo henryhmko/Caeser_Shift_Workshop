63fe8b38d05d4455c85c0b13953183202fb1d9a9
Henry Ko <ko.hyeonmok@berkeley.edu>
2020-03-23 20:20:22 -0700

    Implemented smart caesar decoder

--- Caeser_Shift.py
+++ Caeser_Shift.py
@@ -1,5 +1,6 @@
 
-from PyDictionary import PyDictionary
+import enchant
+dictionary = enchant.Dict("en_US") #yay our very own dictionary
 
 """
 Encode a program for converting normal text to Ceaser shift.
@@ -20,7 +21,6 @@ eng_dict = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8,
             'j':9, 'k':10, 'l':11, 'm':12, 'n':13, 'o':14, 'p':15, 'q':16,
             'r':17, 's':18, 't':19, 'u':20, 'v':21, 'w':22, 'x':23, 'y':24, 'z':25}
 
-#### list(a.keys())[list(a.values()).index(0)]
 
 def caesar_encoder(text, shift):
     text = text.lower()
@@ -54,11 +54,19 @@ def caesar_shift(text, shift, action):
             encoded_str += new_char
     return encoded_str
 
+
 def advanced_caesar_decoder(text):
     all_decipher = []
-    all_decipher_index = []
+
     for shift in eng_dict.keys():
         all_decipher.append(caesar_shift(text, shift, 'decode'))
-    for decipher in all_decipher:
-        string = deciper[0]
-        #need to separate by whitespace
+
+    def correct_words_num(string):
+        all_words = string.split() #splits text by whitespace
+        valid_words = 0
+        for word in all_words:
+            if dictionary.check(word):
+                valid_words += 1
+        return valid_words #number of valid English words within the inputted text
+
+    return max(all_decipher, key = correct_words_num) #return the decipher version with the most number of valid English words
