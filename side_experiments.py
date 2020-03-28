7c629831a166cb3942e0656f153ce7fa4e2f9459
Henry Ko <ko.hyeonmok@berkeley.edu>
2020-03-28 22:15:27 +0900

    started making a encoder for Korean

---
+++ side_experiments.py
@@ -0,0 +1,51 @@
+import random
+import enchant
+
+dictionary = enchant.Dict("en_US")
+
+dictionary.check("???")
+"""
+>>> ord('h')
+104
+>>> ord('a')
+97
+>>> ord('^')
+94
+>>> chr(104)
+'h'
+>>> chr(97)
+'a'
+>>> chr(94)
+'^'
+"""
+
+#make a function to create a big big number(more than 10 digits)
+def big_num(digits):
+    final_num = ''
+    while digits:
+        final_num += str(random.randint(1,9))
+        digits -= 1
+    return int(final_num)
+
+def confusing_char():
+    code = random.randint(46001, 48300)
+    return chr(code)
+
+def eng_to_kor_encoder(text):
+    encoded = ''
+    key = random.randint(45935, 48235)
+    for letter in list(text):
+        encoded += chr(ord(letter) + key)
+    return encoded
+
+
+
+def kor_encoder(text):
+    encoded = ''
+    for char in list(text): #for each korean character
+        encoded += str(ord(letter))
+    return hex(int(encoded)) #convert to hexadecimal
+
+def kor_decoder(text): #"text" should be without strings
+    decoded = ''
+    code = int(text)
